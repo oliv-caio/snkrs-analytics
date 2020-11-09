d07acbf59889c0690c84ca2d65bfbad2fd06980c
Caio Oliveira <oliv.caio@outlook.com>
2020-11-09 10:25:07 -0300

    alfa sneakers funcionando

--- alfasnrks.py
+++ alfasnrks.py
@@ -24,31 +24,13 @@ driver.get(url)
 
 time.sleep(15)
 
-element = driver.find_element_by_xpath("/html/body/main/div/div/div/div[3]/div/div[@class='product-list product-list--per-row-4 product-list--image-shape-natural']")
-html_content = element.get_attribute("outerHTML")
-soup = BeautifulSoup(html_content, 'lxml')
-
-#a_href = soup.find_all('a', href=True)
-
-
-# modelolist = []
-# a_modelo = soup.find_all('div', {'class': 'product-block__title'})
-
-# for a in a_modelo:
-#      modelo = ''.join(a.findAll(text=True))
-#      modelolist.append(modelo)
-
-# precolist = []
-# a_preco = soup.find_all('span', {'class': 'theme-money'})
-
-# for a in a_preco:
-#     preco = ''.join(a.findAll(text=True))
-#     precolist.append(preco)
+site = "https://alfasneakers.com.br/"
 
 for i in range(13):
     element = driver.find_element_by_xpath("/html/body/main/div/div/div/div[3]/div/div[@class='product-list product-list--per-row-4 product-list--image-shape-natural']")
     html_content = element.get_attribute("outerHTML")
     soup = BeautifulSoup(html_content, 'lxml')
+    datetimedb = datetime.datetime.now()
     modelolist = []
     a_modelo = soup.find_all('div', {'class': 'product-block__title'})
 
@@ -57,17 +39,25 @@ for i in range(13):
         modelolist.append(modelo)
 
     precolist = []
-    a_preco = soup.find_all('span', {'class': 'theme-money'})
+    a_preco = soup.find_all('span', {'class': 'price'})
 
     for a in a_preco:
-        preco = ''.join(a.findAll(text=True))
+        preco = str(''.join(a.findAll(text=True)).strip())
         precolist.append(preco)
 
     for i in range(len(modelolist)):
         print(modelolist[i])
         print(precolist[i])
+        sql = """insert into site_reven (reven_html, reven_preco, reven_modelo, reven_data) values(%s, %s, %s, %s)""" 
+        val = (site, precolist[i], modelolist[i], datetimedb)
+        cursor.execute(sql, val)
+        sql = """insert into logs(html_logs, data_hora_logs, tipo_site) values(%s, %s, 0)"""
+        val = (site, datetimedb)
+        cursor.execute(sql, val)
+        db_conn.commit()
+    
     driver.find_element_by_xpath("/html/body/main/div/div/div/div[4]/div/a[@class='next']").click()
-    time.sleep(8)
+    time.sleep(3)
 
 driver.quit()
 db_conn.close()
