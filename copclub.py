1ed2b263d3715d95dcb7ba9e2dbb8ed185c9f285
Caio Oliveira <oliv.caio@outlook.com>
2020-11-17 10:54:43 -0300

    bot copclub funcionando

--- copclub.py
+++ copclub.py
@@ -9,8 +9,8 @@ from selenium.webdriver.firefox.options import Options
 import json
 import datetime
 
-#db_conn = mysql.connector.connect(host="129.213.131.233", port="600", user="root", passwd="root", database="snkrs")
-#cursor = db_conn.cursor()
+db_conn = mysql.connector.connect(host="129.213.131.233", port="600", user="root", passwd="root", database="snkrs")
+cursor = db_conn.cursor()
 
 #1 preparar o conteudo html a partir da url
 binary = FirefoxBinary('C:\\Program Files\\Firefox Developer Edition\\firefox.exe')
@@ -22,7 +22,7 @@ driver = webdriver.Firefox(firefox_binary=binary, executable_path='C:\\geckodriv
 
 driver.get(url)
 
-time.sleep(15)
+time.sleep(6)
 
 site = "CopClub"
 
@@ -39,61 +39,33 @@ for a in a_href:
     href = str(a['href'])
     hreflist.append(href)
 
-modelolist = []
-a_modelo = soup.find_all('a', {'class': 'nome-produto cor-secundaria'})
-
-#for a in a_modelo:
-        #modelo = ''.join(a.findAll(text=True))
-        #modelolist.append(modelo)
-        #print(modelo)
-
-#precolist = []
-a_preco = soup.find_all('strong', {'class': 'preco-promocional cor-principal'})
-
-#for a in a_preco:
-    #preco = str(''.join(a.findAll(text=True)).strip())
-    #precolist.append(preco)
-    #print(preco)
-
-#a_travis = soup.find_all('strong', {'class': 'preco-venda cor-principal '})
-#for a in a_travis:
-#    travis = str(''.join(a.findAll(text=True)).strip())
-#    precolist.append(travis)
-#    #print(travis)
-
 for i in range(len(hreflist)):
     if hreflist[i-1] != hreflist[i] :
         url = hreflist[i-1]
-        #print(url)
         driver.get(url)
-        time.sleep(4)
+        time.sleep(1)
         element = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div[2]/div/div[1]/div[2]/div/div[1]")
-        #element = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div[2]/div")
         html_content = element.get_attribute("outerHTML")
         soup = BeautifulSoup(html_content, 'lxml')
         modelo = soup.find_all('h1', {'class': 'nome-produto titulo cor-secundaria' })
 
         for node in modelo:
-            modelo = str(''.join(node.findAll(text=True)))
-            print(modelo)
+            modelodb = str(''.join(node.findAll(text=True)))
+            print(modelodb)
 
-        element = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div[2]/div/div[1]/div[2]/div/div[5]/div[1]/div[@class='preco-produto destaque-avista ']")
+        element = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div[2]/div/div[1]/div[2]/div[@class='principal']")
         html_content = element.get_attribute("outerHTML")
         soup = BeautifulSoup(html_content, 'lxml')
-        preco = soup.find_all('strong', {'class': 'preco-promocional cor-principal'})
-        for node in preco:
-            preco = str(''.join(node.findAll(text=True)).strip())
-            print(preco)
-        #print(modelolist[i])
-        #print(precolist[i])
+        preco = soup.find('strong', {'class': 'preco-promocional cor-principal'})
+        precodb = str(''.join(preco.find(text=True)).strip())
+        print(precodb)
 
-        # sql = """insert into site_reven (reven_html, reven_preco, reven_modelo, reven_data) values(%s, %s, %s, %s)""" 
-        # val = (site, precolist[i], modelolist[i], datetimedb)
-        # cursor.execute(sql, val)
-        # sql = """insert into logs(html_logs, data_hora_logs, tipo_site) values(%s, %s, 0)"""
-        # val = (site, datetimedb)
-        # cursor.execute(sql, val)
-        # db_conn.commit()
-
+        sql = """insert into site_reven (reven_html, reven_preco, reven_modelo, reven_data) values(%s, %s, %s, %s)""" 
+        val = (site, precodb, modelodb, datetimedb)
+        cursor.execute(sql, val)
+        sql = """insert into logs(html_logs, data_hora_logs, tipo_site) values(%s, %s, 0)"""
+        val = (site, datetimedb)
+        cursor.execute(sql, val)
+        db_conn.commit()
 driver.quit()
-#db_conn.close()
+db_conn.close()
